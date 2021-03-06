cbe3458e2f22ccedc57d81d9f270f8ae83f21485
Arjun Singh <arjun.singh@knoldus.com>
2021-03-06 19:24:38 +0530

    Added Tests for Argument parser

--- cli-parser/src/test/scala/com/knoldus/parser/ArgumentParserTest.scala
+++ cli-parser/src/test/scala/com/knoldus/parser/ArgumentParserTest.scala
@@ -1,5 +1,26 @@
 package com.knoldus.parser
 
-class ArgumentParserTest {
+import org.scalatest.flatspec.AsyncFlatSpec
+import scala.collection.mutable.HashMap
+class ArgumentParserTest extends AsyncFlatSpec {
+
+  """Parsing args["--flag1", "value1", "-f2", "value2", "--flag3", "value3"]""" should "return a corresponding k-v map" in {
+    val argsMap = ArgumentParser.parse(Array("--flag1", "value1", "-f2", "value2", "--flag3", "value3"))
+    assert(argsMap == HashMap("flag1" -> "value1", "f2" -> "value2", "flag3" -> "value3"))
+  }
+
+  """Parsing args ["--flag1", "value1", "-f2" ]""" should "produce IllegalArgumentException" in {
+    assertThrows[IllegalArgumentException] {
+      val argsMap = ArgumentParser.parse(Array("--flag1", "value1", "-f2"))
+    }
+  }
+
+  "-flagName" should "is a flag" in {
+    assert(ArgumentParser.isFlag("-flagName"))
+  }
+
+  "noDashName" should "is not a flag" in {
+    assert(!ArgumentParser.isFlag("noDashName"))
+  }
 
 }
